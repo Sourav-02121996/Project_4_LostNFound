8f03bb6b7ed94720515cffaab713f3bfae7c357e
Puneet Puri <puneet.puri999@gmail.com>
2025-11-02 21:57:27 -0500

    made a few react components and homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <h1>Welcome to LostNFound</h1>
+    <div className="app-container">
+      <Header />
+      <main className="py-3 main-content">
+        <Container>
+        <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
